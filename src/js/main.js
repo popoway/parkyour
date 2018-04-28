cb9899dc5c0ee24fe2758cbfc29ca42aa8cb0db7
popoway <31576265+popoway@users.noreply.github.com>
2018-04-28 12:40:22 -0400

    navbar and footer

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var elem = document.querySelector('.fixed-action-btn');
+var instance = M.FloatingActionButton.init(elem, options);
+
+// Or with jQuery
+
+$(document).ready(function(){
+  $('.fixed-action-btn').floatingActionButton();
+});
+      
