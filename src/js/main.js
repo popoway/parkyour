5918cd9250d64b17e1be31eb14b700485c2d6414
Yasin Ehsan <yasinehsan11@gmail.com>
2018-04-28 14:22:37 -0400

    basic form

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,7 @@ var elem = document.querySelector('.fixed-action-btn');
 var instance = M.FloatingActionButton.init(elem, {
   direction: 'left'
 });
+
+//datepicker
+var datev = document.querySelector('.datepicker');
+var instance = M.Datepicker.init(datev, options);
