f80242daf81b344930153f0caddf7fcfad6866bb
popoway <31576265+popoway@users.noreply.github.com>
2018-04-28 13:42:03 -0400

    add provide page

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,5 @@
+// initialize Floating Action Button
 var elem = document.querySelector('.fixed-action-btn');
-var instance = M.FloatingActionButton.init(elem, options);
-
-// Or with jQuery
-
-$(document).ready(function(){
-  $('.fixed-action-btn').floatingActionButton();
+var instance = M.FloatingActionButton.init(elem, {
+  direction: 'left'
 });
-      
