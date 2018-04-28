347f8d38a7a71b1c8709f4ad59fa212285e0e495
Alberto Franco <alberto.franco86@qmail.cuny.edu>
2018-04-28 12:26:07 -0400

    Added routes for creating and retrieving requests

--- backend/controllers/index.js
+++ backend/controllers/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 router.use('/alt', require('./alt'));
 router.use('/', require('./home'));
+router.use('/request', require('./request'));
 
 
 module.exports = router;
--- backend/controllers/request.js
+++ backend/controllers/request.js
@@ -5,13 +5,30 @@ const RequestController = {
   registerRouter() {
     const router = express.Router();
     router.get('/', this.getRequests);
+    router.post('/', this.createRequest);
 
     return router;
   },
 
   getRequests(req, res) {
-    models.Request.findAll({
-      
-    });
+    models.Request.findAll()
+      .then(allRequests => {
+        res.json(allRequests);
+      })
+      .catch(console.error);
+  },
+
+  createRequest(req, res) {
+    models.Request.create({
+      email: req.body.email,
+      parkName: req.body.parkName,
+      fulfilled: req.body.fulfilled
+    })
+    .then(request => {
+      res.send('Request created successfully');
+    })
+    .catch(console.error);
   }
 }
+
+module.exports = RequestController.registerRouter();
