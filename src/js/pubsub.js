d45dd5a8977ccf5b24e93425ac88c0cc3c885f53
Edmond Weiss <edmondweiss@gmail.com>
2018-04-28 15:35:49 -0400

    Added implementation for publisher subscriber pattern

--- src/js/pubsub.js
+++ src/js/pubsub.js
@@ -1,45 +1,78 @@
 const PubSub = (function() {
-  const events = {};
+  let events = {};
 
-  function publish(path, eventName, {callbackFn, context = null}) {
-    if (typeof eventName !== "string" || typeof callbackFn !== "function") {
-      
+  function publish({ path: path = [] } = {}, eventName, ...data) {
+    if (typeof eventName !== "string") {
+      return;
     }
     const eventLocation = createEventMap(path);
-    addEventToMap(eventLocation, subscription);
-  }
-
-  function createEventMap(path) {
-    path.reduce((accumulator, currentLocation) => {
-      if (events[currentLocation]) {
-        
-      }
+    if (!Array.isArray(eventLocation[eventName])) {
+      eventLocation[eventName] = [];
+    }
+    eventLocation[eventName].forEach(subscription => {
+      subscription.callbackFn.apply(subscription.context, data);
     });
   }
 
-  function subscribe(eventName, callbackFn, ...location) {
-
-  }
-  
-  function unsubscribe() {
-
-  }
-
-  function handleErrors(callbackFn) {
-    return function() {
-      try {
-
-      } catch {
-
+  function createEventMap(path) {
+    let previousLocation = events;
+    for (let i = 0; i < path.length; i++) {
+      let nextLocationName = path[i];
+      if (previousLocation[nextLocationName]) {
+        previousLocation = previousLocation[nextLocationName];
+      } else {
+        previousLocation[nextLocationName] = {};
+        previousLocation = previousLocation[nextLocationName];
       }
     }
+    let finalLocation = previousLocation;
+    return finalLocation;
+  }
+
+  function subscribe(path = [], eventName, {callbackFn: callbackFn, context: context = null} = {}) {
+    if (typeof eventName !== "string" || typeof callbackFn !== "function") {
+      return;
+    }
+    const eventLocation = createEventMap(path);
+    if (!Array.isArray(eventLocation[eventName])) {
+      eventLocation[eventName] = [];
+    }
+    const subscription = { callbackFn: callbackFn, context: context };
+    eventLocation[eventName].push(subscription);
+  }
+  
+  function unsubscribe(path = [], eventName, callbackFn) {
+    if (typeof eventName !== "string" || typeof callbackFn !== "function") {
+      return;
+    }
+    const eventLocation = createEventMap(path);
+    if (!Array.isArray(eventLocation[eventName])) {
+      return;
+    }
+    let eventCallbacks = eventLocation[eventName];
+    eventLocation[eventName] = removeCallback(eventCallbacks, callbackFn);
+
+    if (eventLocation[eventName].length === 0) {
+      delete eventLocation[eventName];
+    } 
+  }
+
+  function removeCallback(callbacksArray, callbackFn) {
+    let callbackFnIndex = callbacksArray.findIndex(hasCallbackFn);
+    if (callbackFnIndex === -1) {
+      return;
+    }
+    return callbacksArray.slice(callbackFnIndex, 1);
+  }
+
+  function hasCallbackFn(subscription) {
+    return subscription.callbackFn === callbackFn;
   }
 
   const api = {
     publish: publish,
     subscribe: subscribe,
-    unsubscribe: unsubscribe
+    unsubscribe: unsubscribe,
+    events: events
   }
-
-  return api;
 })();
